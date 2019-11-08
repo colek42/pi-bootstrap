a65ecb9669eed3eb7f421b3f0b7fca3c138ea395
Cole Kennedy <colek42@gmail.com>
2019-11-07 20:53:55 -0600

    change theme

---
+++ releases/metallb-system/edgerouter.sh
@@ -0,0 +1,10 @@
+configure
+set protocols bgp 64512 parameters router-id 192.168.0.1
+set protocols bgp 64512 neighbor 172.4.20.2 remote-as 64512
+set protocols bgp 64512 neighbor 172.4.20.3 remote-as 64512
+set protocols bgp 64512 neighbor 172.4.20.4 remote-as 64512
+set protocols bgp 64512 neighbor 172.4.20.5 remote-as 64512
+set protocols bgp 64512 redistribute static
+commit
+save
+exit
