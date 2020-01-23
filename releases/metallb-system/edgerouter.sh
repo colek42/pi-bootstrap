eccf8043c2aca96516392b648f530949310fad06
Cole Kennedy <colek42@gmail.com>
2020-01-22 18:04:48 -0600

    change api version

--- releases/metallb-system/edgerouter.sh
+++ releases/metallb-system/edgerouter.sh
@@ -1,8 +1,8 @@
 configure
 set protocols bgp 64512 parameters router-id 192.168.0.1
-set protocols bgp 64512 neighbor 172.4.20.3 remote-as 64512
-set protocols bgp 64512 neighbor 172.4.20.4 remote-as 64512
-set protocols bgp 64512 neighbor 172.4.20.5 remote-as 64512
+set protocols bgp 64512 neighbor 172.4.20.10 remote-as 64512
+set protocols bgp 64512 neighbor 172.4.20.11 remote-as 64512
+set protocols bgp 64512 neighbor 172.4.20.12 remote-as 64512
 set protocols bgp 64512 redistribute static
 commit; save
 exit
